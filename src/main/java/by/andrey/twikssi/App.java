d683e0fa291cee411b208824084fcd73f9c7719e
twikssi <limpbiski15@gmail.com>
2020-09-09 13:38:33 +0300

    app.class improved

--- src/main/java/by/andrey/twikssi/App.java
+++ src/main/java/by/andrey/twikssi/App.java
@@ -1,7 +1,10 @@
 package by.andrey.twikssi;
 
+import java.util.Arrays;
+import java.util.function.*;
+
 public class App {
     public static void main(String[] args) {
-  // main, no args
+    // main, no args
     }
 }
